9ef4af30df00b92ea04ae1541ccf46d300711402
Ariel Wyrobnk <Ariel.Wyrobnik@post.runi.ac.il>
2026-01-03 00:42:17 +0100

    Completed assignment

--- Network.java
+++ Network.java
@@ -133,13 +133,14 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
+        if (userCount == 0) return "Network:";
 
-       String ans = "Network:";
+        String ans = "Network:\n";
 
-       for (int i = 0; i < userCount; i++){
-            ans += users[i] + "\n";
-       }
-
-       return ans;
+        for (int i = 0; i < userCount; i++) {
+            ans += users[i];
+            if (i < userCount - 1) ans += "\n";
+        }
+    return ans;
     }
 }
